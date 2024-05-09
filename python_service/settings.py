9ff006dd825af2988232734bd8d9539b504db14d
ali alef <ali.alefkhani@snapptrip.com>
2024-05-09 14:56:41 +0330

    feat: Updated service and decorators

--- python_service/settings.py
+++ python_service/settings.py
@@ -3,6 +3,7 @@ import os
 from dotenv import dotenv_values
 
 ENVIRONMENT = os.environ.get('ENV', 'local').lower()
-env_vars = dotenv_values(f'../config/{ENVIRONMENT}.env')
+env_vars = dotenv_values(f'./config/{ENVIRONMENT}.env')
 
 DLL_PATH = env_vars.get('DLL_PATH')
+print(DLL_PATH)
--- python_service/utils/decorators.py
+++ python_service/utils/decorators.py
@@ -13,52 +13,55 @@ from utils import (
 class Function:
     def __init__(
             self,
-            path: str,
-            compiler_options: str,
-            name: str,
-            return_type: str,
+            function_name,
+            return_type,
             shard_memory,
+            origin_service,
+            destination_service,
     ):
-        self.path = path
-        self.compiler_options = compiler_options
-        self.name = name
+        self.function_name = function_name
         self.return_type = return_type
         self.shard_memory = shard_memory
+        self.origin_service = origin_service
+        self.destination_service = destination_service
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         id = str(uuid.uuid4())
+
         write_to_shared_memory(
             self.shard_memory,
             json.dumps({
                 "op": "call",
                 "uuid": id,
                 "args": args,
-                "path": self.path,
-                "name": self.name,
+                "destination_service": self.destination_service,
+                "origin_service": self.origin_service,
+                "function_name": self.function_name,
             }),
         )
 
-        while True:
-            data = read_from_shared_memory(self.shard_memory)
-            data = json.loads(data)
-            if data["uuid"] == id and data["op"] == "response":
-                return data["result"]
-
-            time.sleep(0.1)
+        # while True:
+        #     data = read_from_shared_memory(self.shard_memory)
+        #     data = json.loads(data)
+        #     if data["uuid"] == id and data["op"] == "response":
+        #         return data["result"]
+        #
+        #     time.sleep(0.1)
 
 
 class Bridge:
-    def __init__(self, shared_memory):
+    def __init__(self, shared_memory, origin_service):
+        self.origin_service = origin_service
         self.shared_memory = shared_memory
 
-    def js(self, path: str = None, compiler_options: str = None, *args, **kwargs):
+    def js(self, destination_service: str = None, *args, **kwargs):
         def decorator(func):
             func = Function(
-                path=path,
-                compiler_options=compiler_options,
-                name=func.__name__,
+                function_name=func.__name__,
                 return_type=func.__annotations__["return"],
                 shard_memory=self.shared_memory,
+                origin_service=self.origin_service,
+                destination_service=destination_service,
             )
             return func
 
