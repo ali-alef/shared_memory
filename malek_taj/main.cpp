6d6e14d2baaa582a7f571481c6b4d869528b5d19
ali alef <ali.alefkhani@snapptrip.com>
2024-05-24 21:04:29 +0330

    feat: Handled outer requests too

--- malek_taj/main.cpp
+++ malek_taj/main.cpp
@@ -14,6 +14,8 @@ using json = nlohmann::json;
 
 std::mutex SHARED_MEMORY_LOCK;
 
+const std::string LOCAL = "local";
+
 const std::string REQUEST_OPERATION = "request";
 const std::string RESPONSE_OPERATION = "response";
 const std::string LOCK_REQUEST = "lock";
@@ -27,6 +29,57 @@ const void *const METHOD_NOT_ALLOWED = "405";
 const void *const IM_A_TEA_POT = "418";
 const void *const LOCKED = "423";
 const void *const SERVICE_UNAVAILABLE = "503";
+
+void get_outer_requests() {
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, zmq::socket_type::rep);
+    std::string response_route = "tcp://127.0.0.1:" + ENV_MAP["ZMQ_OUTER_PORT"];
+    socket.bind(response_route);
+    std::cout << "outer socket Running on port: " << ENV_MAP["ZMQ_OUTER_PORT"] << std::endl;
+
+    int shm = create_shared_memory();
+
+    while (true) {
+        zmq::message_t request;
+        socket.recv(request, zmq::recv_flags::none);
+        std::string data(static_cast<char*>(request.data()), request.size());
+
+        zmq::message_t response(3);
+        memcpy(response.data(), OK, 3);
+        socket.send(response, zmq::send_flags::none);
+
+        SHARED_MEMORY_LOCK.lock();
+
+        char* data_char = new char[data.size() + 1];
+        std::strcpy(data_char, data.c_str());
+
+        int index = first_empty_spot(shm);
+
+        write_to_shared_memory(shm, data_char, index);
+        SHARED_MEMORY_LOCK.unlock();
+    }
+}
+
+void send_outer_requests(std::string request_route, std::string request_message) {
+    zmq::context_t context(1);
+    zmq::socket_t socket(context, zmq::socket_type::req);
+
+    // Connect to the server
+    socket.connect(request_route);
+
+    // Prepare and send the request message
+    zmq::message_t request(request_message.size());
+    memcpy(request.data(), request_message.data(), request_message.size());
+    socket.send(request, zmq::send_flags::none);
+
+    // Receive the reply message
+    zmq::message_t reply;
+    socket.recv(reply, zmq::recv_flags::none);
+
+    std::string reply_message(static_cast<char*>(reply.data()), reply.size());
+
+    std::cout << "send outer request to route " << request_route << " and got response " << reply_message << std::endl;
+}
 
 void handle_shared_requests() {
     // Initialize ZeroMQ context
@@ -47,7 +100,7 @@ void handle_shared_requests() {
     int times_loop_ran = 0;
     while (true) {
         times_loop_ran++;
-        if(times_loop_ran == 20) {
+        if(times_loop_ran == 40) {
             std::cout << "Removing expired data in set" << std::endl;
             request_set.cleanupExpiredItems();
             times_loop_ran = 0;
@@ -70,6 +123,12 @@ void handle_shared_requests() {
         std::string uuid = parsed_data["uuid"];
         std::string operation = parsed_data["operation"];
         std::stringstream message;
+
+        if(ENV_MAP[service_name] != LOCAL) {
+            send_outer_requests(ENV_MAP[service_name], data);
+            continue;
+        }
+
         message << index << ":" << operation << ":" << service_name << ":" << uuid;
         if (request_set.contains(message.str())) {
             SHARED_MEMORY_LOCK.unlock();
@@ -88,7 +147,7 @@ void handle_shared_lock() {
     zmq::socket_t socket(context, zmq::socket_type::rep);
     std::string response_route = "tcp://127.0.0.1:" + ENV_MAP["ZMQ_RESPONSE_PORT"];
     socket.bind(response_route);
-    std::cout << "Response socket Running  on port: " << ENV_MAP["ZMQ_RESPONSE_PORT"] << std::endl;
+    std::cout << "Response socket Running on port: " << ENV_MAP["ZMQ_RESPONSE_PORT"] << std::endl;
 
     int shm = create_shared_memory();
 
@@ -172,9 +231,11 @@ int main(int argc, char** argv) {
 
     std::thread shared_lock_thread(handle_shared_lock);
     std::thread shared_request_thread(handle_shared_requests);
+    std::thread outer_request_thread(get_outer_requests);
 
     shared_lock_thread.join();
     shared_request_thread.join();
+    outer_request_thread.join();
 
     return 0;
 }
